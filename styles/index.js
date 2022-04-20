e1ee3ab7cb4548d32652933ab9ab567545fa5504
SamyolGH <samalewpp@outlook.com>
2022-04-21 04:02:18 +1000

    some changes idk

--- styles/index.js
+++ styles/index.js
@@ -1,40 +1,36 @@
-
-var timePort = 0;
-var timeDeet = 0;
+// Iterations \\
+var tP = 0;
+var tD = 0;
+var i = 0;
+var it = 0;
 var timeContact = 0;
+// Text Integration \\
 var gitHub = "github";
 var twitter = "twitter";
 var email = "email"
 var myName = "Samuel Drew";
 var myDetails = "computer science student eagar to learn"
 var irllocation = "located brisbane, australia"
+// Element IDs \\
 var na = document.getElementById("displayName");
 var bd = document.getElementById("basicDetails");
 var gl = document.getElementById("GitHubLink");
 var tl = document.getElementById("TwitterLink");
 var el = document.getElementById("EmailLink");
 var ld = document.getElementById("locationDetails");
-var i = 0;
-var it = 0;
-na.innerHTML = "";
-bd.innerHTML = "";
-ld.innerHTML = "";
-
+// Interval Fucntions \\
 var expandName = setInterval(function() {
-    timePort = timePort + 1;
-    na.innerHTML = myName.slice(0, timePort);
+    tP = tP + 1;
+    na.innerHTML = myName.slice(0, tP);
 }, 200);
-
 var expandDetails = setInterval(function() {
-    timeDeet = timeDeet + 1;
-    bd.innerHTML = myDetails.slice(0, timeDeet)
+    tD = tD + 1;
+    bd.innerHTML = myDetails.slice(0, tD)
 }, 60)
-
 var expandLocation = setInterval(function() {
     it = it + 1;
     ld.innerHTML = irllocation.slice(0,it);
-}, 150);
-
+}, 90);
 var expandContact = setInterval(function() {
     i = i + 1;
     tl.innerHTML = twitter.slice(0, i);
