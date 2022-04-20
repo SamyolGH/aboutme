b20af607128c850582854240ea66baa03255207f
SamyolGH <samalewpp@outlook.com>
2022-04-21 04:08:07 +1000

    more javascript cleanup!

--- styles/index.js
+++ styles/index.js
@@ -21,21 +21,7 @@ var tl = document.getElementById("TwitterLink");
 var el = document.getElementById("EmailLink");
 var ld = document.getElementById("locationDetails");
 // Interval Fucntions \\
-var expandName = setInterval(function() {
-    tP = tP + 1;
-    na.innerHTML = pageTexts[3].slice(0, tP);
-}, 200);
-var expandDetails = setInterval(function() {
-    tD = tD + 1;
-    bd.innerHTML = pageTexts[4].slice(0, tD)
-}, 60)
-var expandLocation = setInterval(function() {
-    it = it + 1;
-    ld.innerHTML = pageTexts[5].slice(0,it);
-}, 90);
-var expandContact = setInterval(function() {
-    i = i + 1;
-    tl.innerHTML = pageTexts[1].slice(0, i);
-    gl.innerHTML = pageTexts[0].slice(0, i);
-    el.innerHTML = pageTexts[2].slice(0, i);
-}, 300)
+var expandName = setInterval(function() {tP = tP + 1;na.innerHTML = pageTexts[3].slice(0, tP);}, 200);
+var expandDetails = setInterval(function() {tD = tD + 1;bd.innerHTML = pageTexts[4].slice(0, tD)}, 60)
+var expandLocation = setInterval(function() {it = it + 1;ld.innerHTML = pageTexts[5].slice(0,it);}, 90);
+var expandContact = setInterval(function() {i = i + 1;tl.innerHTML = pageTexts[1].slice(0, i);gl.innerHTML = pageTexts[0].slice(0, i);el.innerHTML = pageTexts[2].slice(0, i);}, 300)
