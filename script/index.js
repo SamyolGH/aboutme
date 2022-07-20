d39e382bf7ea0bbdb29888ee94f0de5b9c4e12c4
SamyolGH <samalewpp@outlook.com>
2022-07-21 01:02:46 +1000

    Neatened up some JS, Added Dragon Scimitar Cursor.
    All for the nostalgia.

--- script/index.js
+++ script/index.js
@@ -1,35 +1,39 @@
-// Iterations \\
-var tP = 0;
-var tD = 0;
-var i = 0;
-var it = 0;
 // Text Integration \\
-var pageTexts = [
+const pageTexts = [
     "github",
     "twitter",
     "email",
     "Samuel Drew",
     "comptuer science student, eager to learn!",
     "located brisbane, australia."
-]
+];
+const htmlElements = [
+    "displayName",
+    "basicDetails",
+    "githubLink",
+    "twitterLink",
+    "emailLink",
+    "locationDetails",
+];
 // Element IDs \\
-var na = document.getElementById("displayName");
-var bd = document.getElementById("basicDetails");
-var gl = document.getElementById("GitHubLink");
-var tl = document.getElementById("TwitterLink");
-var el = document.getElementById("EmailLink");
-var ld = document.getElementById("locationDetails");
+var na = document.getElementById(htmlElements[0]);
+var bd = document.getElementById(htmlElements[1]);
+var gl = document.getElementById(htmlElements[2]);
+var tl = document.getElementById(htmlElements[3]);
+var el = document.getElementById(htmlElements[4]);
+var ld = document.getElementById(htmlElements[5]);
 // Write Text \\
-function writeText(iterator, element, text, speed) {
+function writeText(element, text, speed) {
+    let i = 0;
     setInterval(function() {
-        iterator = iterator + 1;
-        element.innerHTML = text.slice(0, iterator);
+        i += 1;
+        element.innerHTML = text.slice(0, i);
     }, speed);
 };
 // Our function calls \\
-writeText(it, ld, pageTexts[5], 90);
-writeText(tD, bd, pageTexts[4], 60);
-writeText(i, tl, pageTexts[1], 300);
-writeText(i, gl, pageTexts[0], 300);
-writeText(i, el, pageTexts[2], 300);
-writeText(tP, na, pageTexts[3], 200);
+writeText(ld, pageTexts[5], 90);
+writeText(bd, pageTexts[4], 60);
+writeText(tl, pageTexts[1], 300);
+writeText(gl, pageTexts[0], 300);
+writeText(el, pageTexts[2], 300);
+writeText(na, pageTexts[3], 200);
